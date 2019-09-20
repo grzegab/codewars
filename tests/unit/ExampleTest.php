07617d04d40238ae4d558098ae2790747f88102b
Grzegab <greg@grzegab.eu>
2019-09-20 19:51:31 +0200

    init commit

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,23 @@
+<?php namespace App\Tests;
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \App\Tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+        $this->assertTrue(true);
+    }
+}
